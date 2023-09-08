e4ba9a3086df2af3729d7807d7093324a135026e
Aditya Vadhavkar <aditya.vadhavkar12@gmail.com>
2023-09-08 11:46:02 -0400

    Added functionality to create users.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -34,8 +34,15 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.email)
 
     def test_edit_user_page(self):
-        """Test for the add / edit user page"""
+        """Test for the edit user page"""
         url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test for the create user page"""
+        url = reverse("admin:core_user_add")
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
